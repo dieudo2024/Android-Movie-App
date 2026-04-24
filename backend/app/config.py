07b784e3113fd2802bef79e0ca6e7ac750cd4b98
Emmanuelle <EmmanuelleIsis.Mbeug@stu.crc-lennox.qc.ca>
2026-04-24 11:46:33 -0400

    Added the config.py to the the app folder

---
+++ backend/app/config.py
@@ -0,0 +1,16 @@
+# Settings to use for the database connection
+
+# The network address of the MySQL Server
+DB_HOST="localhost"
+
+# The MySQL username to connect to the database
+DB_USER="root"
+
+# The MySQL password
+DB_PASSWORD="root123"
+
+# The name of the database connection
+DB_NAME="movie_db"
+
+# The port number to the MySQL Server
+DB_PORT=3306
